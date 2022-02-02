ae5c6c1cac8659ea27179db7b85b262c55eecb21
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-02-01 18:33:05 -0600

    coding challenge complete.

--- challenge.js
+++ challenge.js
@@ -277,3 +277,30 @@ function checkWinner(avgDolphins,avgKoalas) {
 checkWinner(avgDolphins,avgKoalas);
 */
 
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+// Your tasks:
+// 1. Writeafunction'calcTip'thattakesanybillvalueasaninputandreturns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+// 2. Andnowlet'susearrays!Socreateanarray'bills'containingthetestdata below
+// 3. Createanarray'tips'containingthetipvalueforeachbill,calculatedfrom the function you created before
+// 4. Bonus:Createanarray'total'containingthetotalvalues,sothebill+tip Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+// GOOD LUCK 😀
+let tip;
+let bills = [125,555,44];
+let tips = [calcTip(125),calcTip(555),calcTip(44)];
+let totals = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]]
+
+function calcTip(billValue){
+  if(billValue >= 50 && billValue <= 300){
+     tip = billValue * 0.15
+  }else{
+     tip = billValue * 0.20
+  }
+  return tip
+  
+}
+console.log(calcTip(20))
+console.log(bills)
+console.log(tips)
+console.log(totals)
